04347f87a7e78c7cebd80b039e534e8432d1f14d
Blechlawine <marczinser@gmx.de>
2023-10-27 10:49:33 +0200

    feat: default export on routes is GET

--- lib.ts
+++ lib.ts
@@ -1,12 +1,12 @@
 import { AsyncLocalStorage } from "async_hooks";
 import { createContext } from "unctx";
 
-type Route<TContext> = {
-	GET?: (ctx: TContext) => Promise<Response>;
-	POST?: (ctx: TContext) => Promise<Response>;
-	PUT?: (ctx: TContext) => Promise<Response>;
-	DELETE?: (ctx: TContext) => Promise<Response>;
-};
+type Route<TContext> =
+	| {
+			[key: string]: (ctx: TContext) => Promise<Response>;
+	  } & {
+			default?: (ctx: TContext) => Promise<Response>;
+	  };
 
 export type InferContext<T extends Server> = T extends Server<infer C>
 	? C
@@ -49,14 +49,23 @@ export class Server<
 			throw new Error(`No matching route for ${req.method} ${req.url}`);
 		}
 		const imported = (await import(matched.filePath)) as Route<TContext>;
-		const method = req.method as keyof Route<TContext>;
+		const method = req.method;
 		if (method in imported) {
 			const handler = imported[method];
 			if (handler)
 				return context.call(ctx, () => {
 					return handler(ctx);
 				});
+		} else if (
+			method.toLowerCase() === "get" &&
+			"default" in imported &&
+			imported.default !== undefined
+		) {
+			const handler = imported.default;
+			return context.call(ctx, () => {
+				return handler(ctx);
+			});
 		}
-		throw new Error("Method not allowed");
+		throw new Error(`Method not allowed: ${method} on ${req.url}`);
 	}
 }
--- pages/index.ts
+++ pages/index.ts
@@ -1,6 +1,6 @@
 import { type Context, useCtx } from "..";
 
-export function GET(context: Context) {
+export default function GET(context: Context) {
 	// The context is passed into this function as the first parameter, or you can use it anywhere with useCtx (though you need to export that manually from your server to get typesafety)
 	const ctx = useCtx();
 	return new Response("Helo world");
